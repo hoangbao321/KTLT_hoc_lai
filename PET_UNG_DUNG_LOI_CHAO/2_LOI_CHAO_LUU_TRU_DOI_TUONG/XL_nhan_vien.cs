d985a194f8090101cc2358fd3617449f4cc02a8e
Nguyen Le Hoang Bao <nglehoangbao@gmail.com>
2021-12-24 16:50:14 +0700

    12242021_nglehoangbao

--- PET_UNG_DUNG_LOI_CHAO/2_LOI_CHAO_LUU_TRU_DOI_TUONG/XL_nhan_vien.cs
+++ PET_UNG_DUNG_LOI_CHAO/2_LOI_CHAO_LUU_TRU_DOI_TUONG/XL_nhan_vien.cs
@@ -16,24 +16,41 @@ public class XL_nhan_vien
 {
     public static nhanvien doc(string Ma_so)
     {
-        string file_path = HttpContext.Current.Server.MapPath("\\Du_lieu");
-        string[] file_name = Directory.GetFiles(file_path, "*.json");
+        #region cách 1 
+        //string file_path = HttpContext.Current.Server.MapPath("\\Du_lieu");
+        //string[] file_name = Directory.GetFiles(file_path, "*.json");
+        //for (int i  = 0; i < file_name.Length;i++)
+        //{
+        //    var duong_dan = file_name[i];
+        //    StreamReader f = new StreamReader(duong_dan);
+        //    string content = f.ReadLine();
+        //    f.Close();
+        //    var nv = JsonConvert.DeserializeObject<nhanvien>(content);
+        //    if (nv.Ma_so==Ma_so)
+        //    {
+        //        nv.So_lan_Chao++;
+        //        ghi(duong_dan, nv);
+        //        return nv;
+        //    }
+        //}
+        //return null;
+        #endregion
 
-        for (int i  = 0; i < file_name.Length;i++)
+        #region cách 2
+        string file_path = HttpContext.Current.Server.MapPath($"\\Du_lieu\\{Ma_so}.json");
+        if( File.Exists(file_path))
         {
-            var duong_dan = file_name[i];
-            StreamReader f = new StreamReader(duong_dan);
+            StreamReader f = new StreamReader(file_path);
             string content = f.ReadLine();
             f.Close();
             var nv = JsonConvert.DeserializeObject<nhanvien>(content);
-            if(nv.Ma_so==Ma_so)
-            {
-                nv.So_lan_Chao++;
-                ghi(duong_dan, nv);
-                return nv;
-            }
+            nv.So_lan_Chao++;
+            ghi(file_path, nv);
+            return nv;
         }
         return null;
+        #endregion
+
     }
     public static void ghi(string duong_dan, nhanvien nv)
     {
