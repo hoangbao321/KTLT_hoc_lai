4b5155130e383ddcaa5aa3056b3544ab46b3d4f8
Nguyen Le Hoang Bao <nglehoangbao@gmail.com>
2021-12-24 18:29:10 +0700

    update

--- PET_UNG_DUNG_LOI_CHAO/3_LOI_CHAO_LUU_TRU_DANH_SACH/XL_nhan_vien.cs
+++ PET_UNG_DUNG_LOI_CHAO/3_LOI_CHAO_LUU_TRU_DANH_SACH/XL_nhan_vien.cs
@@ -16,45 +16,32 @@ public class XL_nhan_vien
 {
     public static nhanvien doc(string Ma_so)
     {
-        #region cách 1 
-        //string file_path = HttpContext.Current.Server.MapPath("\\Du_lieu");
-        //string[] file_name = Directory.GetFiles(file_path, "*.json");
-        //for (int i  = 0; i < file_name.Length;i++)
-        //{
-        //    var duong_dan = file_name[i];
-        //    StreamReader f = new StreamReader(duong_dan);
-        //    string content = f.ReadLine();
-        //    f.Close();
-        //    var nv = JsonConvert.DeserializeObject<nhanvien>(content);
-        //    if (nv.Ma_so==Ma_so)
-        //    {
-        //        nv.So_lan_Chao++;
-        //        ghi(duong_dan, nv);
-        //        return nv;
-        //    }
-        //}
-        //return null;
-        #endregion
 
-        #region cách 2
-        string file_path = HttpContext.Current.Server.MapPath($"\\Du_lieu\\{Ma_so}.json");
+        #region
+        string file_path = HttpContext.Current.Server.MapPath($"\\Du_lieu\\danh_sach_nhan_vien.json");
         if( File.Exists(file_path))
         {
             StreamReader f = new StreamReader(file_path);
             string content = f.ReadLine();
             f.Close();
-            var nv = JsonConvert.DeserializeObject<nhanvien>(content);
-            nv.So_lan_Chao++;
-            ghi(file_path, nv);
-            return nv;
+            var  ds_nhan_vien = JsonConvert.DeserializeObject<List<nhanvien>>(content);
+            foreach (nhanvien nv in ds_nhan_vien)
+            {
+                if(nv.Ma_so==Ma_so)
+                {
+                    nv.So_lan_Chao++;
+                    ghi(file_path, nv);
+                    return nv;
+                }
+            }
         }
         return null;
         #endregion
 
     }
-    public static void ghi(string duong_dan, nhanvien nv)
+    public static void ghi(string filepath, nhanvien nv)
     {
-        StreamWriter f = new StreamWriter(duong_dan);
+        StreamWriter f = new StreamWriter(filepath);
         string content = JsonConvert.SerializeObject(nv);
         f.WriteLine(content);
         f.Close();
